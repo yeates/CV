abe174a2080235f18fdb25f8b6c1511142f75187
yeates <yeates98@hotmail.com>
2017-07-08 20:17:08 +0800

    python用到的文件

---
+++ 机读卡识别计分/python实现/CardDetect.py
@@ -0,0 +1,11 @@
+# -*- coding:utf8 -*-
+# -*- coder：yeates -*-
+# -*-  -*-
+
+import numpy
+from pylab import *
+from PIL import Image
+import cv2
+import matplotlib
+import math
+from scipy import signal
